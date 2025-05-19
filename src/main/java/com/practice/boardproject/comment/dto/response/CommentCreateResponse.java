8a235a5eeac66467452c3daf83987c945d5dc656
Jimin730 <dnddnd120@naver.com>
2025-05-19 15:37:46 +0900

    refactor: 코드 수정

--- src/main/java/com/practice/boardproject/comment/dto/response/CommentCreateResponse.java
+++ src/main/java/com/practice/boardproject/comment/dto/response/CommentCreateResponse.java
@@ -3,7 +3,7 @@ package com.practice.boardproject.comment.dto.response;
 import java.time.LocalDateTime;
 
 public record CommentCreateResponse(
-        String comment,
+        String content,
         String author,
         LocalDateTime createdAt
 ) {
